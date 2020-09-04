19d6f167c5cbc6204228a7ee4a8fa5a872227b70
Lacie <lacie.huang@klearthink.com>
2020-09-04 21:50:49 +0800

    project init

---
+++ android/app/src/main/kotlin/com/lacie/flutter_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lacie.flutter_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/lacie/flutterweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lacie.flutterweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
